8399cde0c1348930b54551194c877ef42f6cfdbd
Михаил Долгов <dolgov@acs.local>
2017-12-28 14:22:11 +0600

    create TelegramBot class extends TelegramLongPollingBot

---
+++ src/main/java/ru/dolgov/telegrambot/telegrambot/TelegramBot.java
@@ -0,0 +1,24 @@
+package ru.dolgov.telegrambot.telegrambot;
+
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class TelegramBot extends TelegramLongPollingBot{
+
+	public String getBotUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void onUpdateReceived(Update arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getBotToken() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
